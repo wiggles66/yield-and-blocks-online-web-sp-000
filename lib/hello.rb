94b0bf27a6d50113e19d08639110a12963cff702
Michael Waide <mvwaide@gmail.com>
2020-06-25 19:06:58 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,22 +1,15 @@
 def hello_t(array)
-  if block_given?
     i = 0
-
     while i < array.length
       yield(array[i])
       i = i + 1
     end
-
     array
-  else
-    puts "Hey! No block was given!"
-  end
 end
-The block_given? method returns true if
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
-     puts "Hi, #{name}"
-   end
- end
+    puts "Hi, #{name}"
+  end
+end
